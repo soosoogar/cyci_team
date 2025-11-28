defa97b0835a1c457fa66a55d1f9b7a6f1647e87
rlagudwnsdla <rlagudwnsdla1114@gmail.com>
2025-11-28 14:49:25 +0900

    e

--- src/main/java/kr/soft/git/MemberController.java
+++ src/main/java/kr/soft/git/MemberController.java
@@ -1,11 +1,18 @@
 package kr.soft.git;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 public class MemberController {
 
     @RestController
     public static class MemberControllerResource {
 
+        @GetMapping("/rmadydlf")
+        public void rmadydlf() {
+            log.info("rmadydlfdlsepdyd");
+        }
     }
 }
