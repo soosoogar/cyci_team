dedb82fac281edbf077326d7fd5ffe4521a52585
KHS <khsoo8546@gmail.com>
2025-11-28 14:40:55 +0900

    getMapping 추가

--- src/main/java/kr/soft/git/MemberController.java
+++ src/main/java/kr/soft/git/MemberController.java
@@ -1,11 +1,15 @@
 package kr.soft.git;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
 public class MemberController {
 
-    @RestController
-    public static class MemberControllerResource {
-
+    @GetMapping("/sosu")
+    public void sosu() {
+        log.info("sosu");
     }
 }
