84ca2ff23a95b6932e0acbd1b33fac48e7c2cbb5
KHS <khsoo8546@gmail.com>
2025-11-28 11:38:56 +0900

    first

---
+++ src/test/java/kr/soft/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.soft.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
