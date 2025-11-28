175693a4a3642108a17f6f3734176dcb3e3dc8e1
HHJ <hhj2025>
2025-11-28 15:07:44 +0900

    Aa

--- src/main/java/kr/soft/git/MemberController.java
+++ src/main/java/kr/soft/git/MemberController.java
@@ -1,11 +1,18 @@
 package kr.soft.git;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 public class MemberController {
 
     @RestController
     public static class MemberControllerResource {
 
+    }
+
+    @GetMapping("/Aa")
+    public String aa() {
+        return "aa";
     }
 }
