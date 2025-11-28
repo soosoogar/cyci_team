5112ea7c3e8f1acf6b5bf1ec1e7c334c1250da46
KHS <khsoo8546@gmail.com>
2025-11-28 14:33:15 +0900

    controller 추가

---
+++ src/main/java/kr/soft/git/MemberController.java
@@ -0,0 +1,11 @@
+package kr.soft.git;
+
+import org.springframework.web.bind.annotation.RestController;
+
+public class MemberController {
+
+    @RestController
+    public static class MemberControllerResource {
+
+    }
+}
